8d766cc341ada100671fe330b2815a415cc03dc3
Nicolás Bentancor <nicolasbentancor1996@hotmail.com>
2022-07-18 23:06:41 -0300

    Cambios en los montajes, todo ok

--- genDockerContainers.sh
+++ genDockerContainers.sh
@@ -22,7 +22,7 @@ echo Permisos a los scripts que se van a montar
 chmod +x levantardockernode1.sh levantardockernode2.sh levantardockernode3.sh levantardockernode4.sh
 
 echo Ejecutar contenedor con servicio FTP-alpine
-docker run --rm -d --network testNetwork --ip 192.168.10.6 -p 8021:8021 -p 21000-21010:21000-21010 -v /home/testingEnvironment:/home/testingEnvironment -e USERS="ftpNode|ftp1234" -e ADDRESS="192.168.0.100" vsftpdalpine3.15:1.0
+docker run --rm -d --network testNetwork --ip 192.168.10.6 -p 8021:8021 -p 21000-21010:21000-21010 -v c:/testingEnvironmentBlog:/home/testingEnvironment -e USERS="ftpNode|ftp1234|/home/testingEnvironment|1000" -e ADDRESS="192.168.0.100" vsftpdalpine3.15:1.0
 echo Ejecutar contenedores Wildfly
 docker run -itd --network testNetwork --ip 192.168.10.2 -v c:/testingEnvironmentBlog/standalone-full-ha-dockernode1.xml:/opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml -v c:/testingEnvironmentBlog/levantardockernode1.sh:/opt/jboss/wildfly/levantarWildfly.sh -v c:/testingEnvironmentBlog/cacerts:/opt/java/openjdk/lib/security/cacerts -v c:/testingEnvironmentBlog/deploymentDockerNode1:/opt/jboss/wildfly/standalone/deployments --publish 8087:8087 --publish 9997:9997 -d alpine3.14wildfly26.1.1jdk17:1.0 
 docker run -itd --network testNetwork --ip 192.168.10.3 -v c:/testingEnvironmentBlog/standalone-full-ha-dockernode2.xml:/opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml -v c:/testingEnvironmentBlog/levantardockernode2.sh:/opt/jboss/wildfly/levantarWildfly.sh -v c:/testingEnvironmentBlog/cacerts:/opt/java/openjdk/lib/security/cacerts -v c:/testingEnvironmentBlog/deploymentDockerNode2:/opt/jboss/wildfly/standalone/deployments --publish 8187:8187 --publish 10097:10097 -d alpine3.14wildfly26.1.1jdk17:1.0 
